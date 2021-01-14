389d148094b285772659f070f27fd7dd5bfdadc6
Nega-Tiv <62760206+Nega-Tiv@users.noreply.github.com>
2021-01-14 20:48:05 +0300

    F-social
    Rassengan add

---
+++ Fsocial/Items/Rassengan/Rassengan.cs
@@ -0,0 +1,41 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Fsocial.Items.Rassengan
+{
+	public class Rassengan : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("Rassengan"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Rasseeeeengan!!!");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 500;
+			item.melee = true;
+			item.width = 22;
+			item.height = 22;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 3;
+			item.knockBack = 10;
+			item.value = 10000;
+			item.rare = 13;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+            item.useTurn = true; //может ли персонаж обернуться во время работы
+			item.crit = 100;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.StoneBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
